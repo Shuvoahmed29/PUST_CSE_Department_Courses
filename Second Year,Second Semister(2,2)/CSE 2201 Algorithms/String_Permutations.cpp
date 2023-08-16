bbb88b90aa7c747d64d4092fa2b3c7a4fcea661d
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2023-08-16 16:42:25 +0600

    String_Permutations.cpp

--- Second Year,Second Semister(2,2)/CSE 2201 Algorithms/String_Permutations.cpp
+++ Second Year,Second Semister(2,2)/CSE 2201 Algorithms/String_Permutations.cpp
@@ -1,29 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-void code(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif 
+void solve(string str,vector<string> &vec,int idx){
+	if(idx>=str.size()){
+		vec.push_back(str);
+		return;
+	}
+	for(int i=idx;i<str.size();i++){
+		swap(str[idx],str[i]);
+		solve(str,vec,idx+1);
+		swap(str[idx],str[i]);
+	}
 }
 int main(){
-	code();
-	int tt;	cin >> tt;
-	double fact[26+2];
-	fact[0] = fact[1] = 1;
-	for(int i=2;i<=26;i++) fact[i] = i*fact[i-1];
-	while(tt--){
-		string str;	cin >> str;
-		double n;	cin >> n;
-		n--;
-		double total = fact[str.size()];
-		string ans = "";
-		for(int i=0;i<str.size();i++){
-			total = total/(i+1);
-			int idx = n/total;
-			n = fmod(n,total);
-			ans.insert(ans.begin()+idx,str[i]);
-		}
-		cout << ans << endl;
+	string str;	cin >> str;
+	int idx = 0;
+	vector<string> vec;
+	solve(str,vec,idx);
+	for(auto it:vec){
+		cout << it << " ";
 	}
+	cout <<"\nTotal Permutation: "<< vec.size() << endl;
 }
